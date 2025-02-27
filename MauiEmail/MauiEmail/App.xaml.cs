490c5a68b3f37a09d01ff53eced027f3d92d7a2d
LauraaMolinaa <123187064+LauraaMolinaa@users.noreply.github.com>
2025-02-26 20:13:20 -0500

    added views

--- MauiEmail/MauiEmail/App.xaml.cs
+++ MauiEmail/MauiEmail/App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace MauiEmail
+﻿using EmailConsoleApp.Classes;
+using MailKit.Security;
+using MauiEmail.Interfaces;
+using MauiEmail.Models;
+
+namespace MauiEmail
 {
     public partial class App : Application
     {
+        public static MailConfig _mailConfig = new MailConfig()
+        {
+            EmailAddress = "laura.appdev3@gmail.com",
+            Password = "fltuzvtcslduhduo",
+            ReceiveHost = "imap.gmail.com",
+            ReceivePort = 993,
+            ReceiveSocketOptions = SecureSocketOptions.SslOnConnect,
+            SendHost = "smtp.gmail.com",
+            SendPort = 465,
+            SendSocketOptions = SecureSocketOptions.SslOnConnect,
+        }; 
+
+        public static EmailService _emailService = new EmailService(_mailConfig); 
         public App()
         {
             InitializeComponent();
---
+++ MauiEmail/MauiEmail/Models/MailConfig.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Security;
+using MauiEmail.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiEmail.Models
+{
+    public class MailConfig : IMailConfig
+    {
+        public MailConfig()
+        {
+
+        }
+
+        // Authentication
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+
+        // Receiving Service
+        public string ReceiveHost { get; set; }
+        public SecureSocketOptions ReceiveSocketOptions { get; set; }
+        public int ReceivePort { get; set; }
+
+        // Sending Service
+        public string SendHost { get; set; }
+        public int SendPort { get; set; }
+        public SecureSocketOptions SendSocketOptions { get; set; }
+
+        // OAuth 2.0 (optional)
+        public string OAuth2ClientId { get; set; }
+        public string OAuth2ClientSecret { get; set; }
+        public string OAuth2RefreshToken { get; set; }
+
+    }
+}
